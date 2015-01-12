1764cb8df272113587c322821db2b9436d4e7616
Jens Wille <jens.wille@gmail.com>
2015-01-12 09:29:15 +0100

    ext/filemagic/extconf.rb: Compile with static libgcc and gnurx for Windows compatibility.
    Related to #10.

--- ext/filemagic/extconf.rb
+++ ext/filemagic/extconf.rb
@@ -1,7 +1,12 @@
 require 'mkmf'
 
 $CFLAGS << ' -Wall' if ENV['WALL']
+$LDFLAGS << ' -static-libgcc'
+
 dir_config('magic')
+dir_config('gnurx')
+
+have_library('gnurx')
 
 if have_library('magic', 'magic_open') && have_header('magic.h')
   have_header('file/patchlevel.h')
