0f80e2b67ec48135d9457655f902b2ad83a02343
Rick Ohnemus <rick_ohnemus@acm.org>
2022-09-19 16:58:35 -0700

    Backward compatibility with older Mac changes.
    This is slightly different from the older changes because it allows
    --with-magic-* to override the defaults in this file.

--- ext/filemagic/extconf.rb
+++ ext/filemagic/extconf.rb
@@ -1,10 +1,32 @@
 require 'mkmf'
 
+hdr_dirs = lib_dirs = nil
+
+if RbConfig::CONFIG['host_os'] =~ /darwin/
+  unless with_config('magic-dir') || with_config('magic-include')
+    hdr_dirs = [
+      '/opt/local/include', # MacPorts
+      '/usr/local/include', # compiled from source and Homebrew
+      '/opt/homebrew/include', # compiled from source and Homebrew (ARM based Macs)
+      '/usr/include',       # system
+    ]
+  end
+
+  unless with_config('magic-dir') || with_config('magic-lib')
+    lib_dirs = [
+      '/opt/local/lib', # MacPorts
+      '/usr/local/lib', # compiled from source and Homebrew
+      '/opt/homebrew/lib', # compiled from source and Homebrew (ARM based Macs)
+      '/usr/lib',       # system
+    ]
+  end
+end
+
 $CFLAGS << ' -Wall' if ENV['WALL']
 $LDFLAGS << ' -static-libgcc' if RUBY_PLATFORM =~ /cygwin|mingw|mswin/
 
-dir_config('magic')
-dir_config('gnurx')
+dir_config('magic', hdr_dirs, lib_dirs)
+dir_config('gnurx', hdr_dirs, lib_dirs)
 
 have_library('gnurx')
 
@@ -13,5 +35,5 @@ if have_library('magic', 'magic_open') && have_header('magic.h')
   have_header('file/patchlevel.h')
   create_makefile('filemagic/ruby_filemagic')
 else
-  abort '*** ERROR: missing required library to compile this module'
+  abort '*** ERROR: missing required header and/or library to compile this module'
 end
