c246f674f545ba3b752e7ca1a03a69d3594a550d
Rick Ohnemus <rick_ohnemus@acm.org>
2023-12-07 09:51:12 -0800

    Revert back to original changes without darwin checks.
    Looked at MANY gems and very few set paths for headers and/or libraries.
    The vast majority expect --with-* options to be used when installing gems
    that use things in non-standard places.
    
    Old commit log entry:
    
    Don't use default include and library directories with dir_config
    
    Using default directories in publicly available gems can cause problems
    because the directories are used when building unless all of them are
    overridden. For this gem it means running "gem install" with
    "--with-magic-dir=..." and "--with-gnurx-dir=..." (yes, even if gnurx
    isn't installed on the system). Kind of annoying to have to look at a
    gem's extconf.rb to see what dir_config calls are being made and what
    needs to be overridden because of defaults that conflict with how ruby
    was configured/installed.
    
    The above discovery was prompted by having libmagic installed in both
    the system default location and a non-standard location (that ruby uses
    for a bunch of other third-party libraries). The following warning was
    issued when trying to use the gem unless the gem was installed with
    both '--with-magic-dir=..." and "-with-gnurx-dir=...":
    
      FileMagic v0.7.3: compiled magic version [5.40] does not match with
      shared library magic version [5.43]

--- ext/filemagic/extconf.rb
+++ ext/filemagic/extconf.rb
@@ -1,32 +1,10 @@
 require 'mkmf'
 
-hdr_dirs = lib_dirs = nil
-
-if RbConfig::CONFIG['host_os'] =~ /darwin/
-  unless with_config('magic-dir') || with_config('magic-include')
-    hdr_dirs = [
-      '/opt/local/include', # MacPorts
-      '/usr/local/include', # compiled from source and Homebrew
-      '/opt/homebrew/include', # compiled from source and Homebrew (ARM based Macs)
-      '/usr/include',       # system
-    ]
-  end
-
-  unless with_config('magic-dir') || with_config('magic-lib')
-    lib_dirs = [
-      '/opt/local/lib', # MacPorts
-      '/usr/local/lib', # compiled from source and Homebrew
-      '/opt/homebrew/lib', # compiled from source and Homebrew (ARM based Macs)
-      '/usr/lib',       # system
-    ]
-  end
-end
-
 $CFLAGS << ' -Wall' if ENV['WALL']
 $LDFLAGS << ' -static-libgcc' if RUBY_PLATFORM =~ /cygwin|mingw|mswin/
 
-dir_config('magic', hdr_dirs, lib_dirs)
-dir_config('gnurx', hdr_dirs, lib_dirs)
+dir_config('magic')
+dir_config('gnurx')
 
 have_library('gnurx')
 
